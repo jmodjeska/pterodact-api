4d604d422b56ffcc939c40a800d9d67f074696eb
jmodjeska <github@daqron.com>
2016-05-12 18:44:55 -0700

    Add JWT auth

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,16 @@
 class CoursesController < ApplicationController
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :restrict_access
+
   def index
     courses = Course.all
     render json: courses, status: 200
   end
+
+  private
+    def restrict_access
+      authenticate_or_request_with_http_token do |token, options|
+        ApiKey.exists?(access_token: token)
+      end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
 Course.destroy_all
+User.destroy_all
 
-100.times do
+10.times do
   course = Course.create(
     name: Faker::App.name,
     catalog: Faker::Lorem.characters(8).upcase,
     description: Faker::Company.catch_phrase
   )
 end
+
+User.create!(
+        name: 'Pterodactyl Jones',
+        email: 'pterodact@example.com',
+        password: 'pterodact',
+        password_confirmation: 'pterodact'
+)
